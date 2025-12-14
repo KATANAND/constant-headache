a135bf79c087168689eecbff648eb8661903bba0
KATANAND <153915345+KATANAND@users.noreply.github.com>
2025-12-15 00:44:25 +0300

    make pointers to pointers pointers to pointers to pointers

--- const.cpp
+++ const.cpp
@@ -1,11 +1,11 @@
 struct Const {
-    const char **const chor(const char **const chor, const char **const (**chcr)(const char **const)) const {
+    const char ***const chor(const char ***const chor, const char ***const (***chcr)(const char ***const)) const {
         return chor;
     }
 };
 int main() {
     const Const Chor;
-    const char **const chor = (const char **const)0;
-    const char **const (**Const)(const char **const);
+    const char ***const chor = (const char ***const)0;
+    const char ***const (***Const)(const char ***const);
     Chor.chor(chor, Const);
 }
